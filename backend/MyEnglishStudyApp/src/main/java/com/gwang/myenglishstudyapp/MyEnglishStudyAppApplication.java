749664f1f8bc4a9fe00d99a7552c4e7798351a50
EON-DC <mun0392@gmail.com>
2023-02-22 02:52:01 +0900

    init

---
+++ backend/MyEnglishStudyApp/src/main/java/com/gwang/myenglishstudyapp/MyEnglishStudyAppApplication.java
@@ -0,0 +1,13 @@
+package com.gwang.myenglishstudyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEnglishStudyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEnglishStudyAppApplication.class, args);
+	}
+
+}
---
+++ backend/MyEnglishStudyApp/src/test/java/com/gwang/myenglishstudyapp/MyEnglishStudyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwang.myenglishstudyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEnglishStudyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
